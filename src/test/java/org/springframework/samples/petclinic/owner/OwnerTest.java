23a04d54a183814d21350c86d7d1ed8ebf17c270
TheRealRondon <amirhossein2324236@gmail.com>
2021-10-12 18:28:14 +0330

    Added first test for telepone get set in OwnerTest.

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -1,7 +1,22 @@
 package org.springframework.samples.petclinic.owner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class OwnerTest {
+
+    private Owner owner;
+
+    @BeforeEach
+    public void setup() {
+        owner = new Owner();
+    }
+
+    @Test
+    public void testSetGetTelephone() {
+        owner.setTelephone("09392324236");
+        assertEquals(owner.getTelephone(), "09392324236");
+    }
 
 }
